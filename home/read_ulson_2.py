f54a318ae47be81ddb2dce3c25ab0723bb05cc0c
Rams <ramageorgius@gmail.com>
2026-07-18 14:54:03 +0700

    ini update baru

--- home/read_ulson_2.py
+++ home/read_ulson_2.py
@@ -16,7 +16,7 @@ from std_msgs.msg import Int32MultiArray
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-TRIG = [19, 6, 22, 17, 21, 16, 23]
+TRIG = [19, 6, 22, 27, 21, 16, 23]
 ECHO = [26, 13, 5, 25, 20, 12, 24]
 
 SENSOR_NAMES = [
