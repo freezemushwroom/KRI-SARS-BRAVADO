af90cfdb2d5ef6fb9b8004defe375260eb54fd2e
Rams <ramageorgius@gmail.com>
2026-07-27 15:41:57 +0700

    ini update baru

--- home/simple_navigation_1.py
+++ home/simple_navigation_1.py
@@ -289,12 +289,12 @@ class NavigationController(Node):
                     self.correcting_state = True
                     self.get_logger().info("Leaning Right(q3)")
                 
-                elif self.deviation_right_dist < 8:
+                elif self.deviation_right_dist < 8 and self.deviation_right_dist > 1:
                     self.rightdist_dev_value = self.deviation_right_dist
                     self.correcting_state_strafe = True
                     self.get_logger().info("Close to right wall")
                 
-                elif self.deviation_left_dist < 8:
+                elif self.deviation_left_dist < 8 and self.deviation_left_dist > 1:
                     self.leftdist_dev_value = self.deviation_left_dist
                     self.correcting_state_strafe = True
                     self.get_logger().info("Close to left wall")
